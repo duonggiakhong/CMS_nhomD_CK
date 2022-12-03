09d2983bce4a84a5fa5dda9aae55554e4bec26db
cucdaucho@gmail.com <cucdaucho@gmail.com>
2022-12-03 15:01:20 +0700

    update

--- wp-content/themes/jobscout/single-job_listing.php
+++ wp-content/themes/jobscout/single-job_listing.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The template for displaying all single job posts.
  *
@@ -8,17 +9,17 @@
  */
 get_header(); ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main">
+<div id="primary" class="content-area">
+	<main id="main" class="site-main">
 
-			<?php
-			while ( have_posts() ) : the_post();
-		        get_template_part( 'template-parts/content', 'job-single' );
+		<?php
+		while (have_posts()) : the_post();
+			get_template_part('template-parts/content', 'job-single');
 
-			endwhile; // End of the loop.
-			?>
-		</main><!-- #main -->
-	</div><!-- #primary -->
+		endwhile; // End of the loop.
+		?>
+	</main><!-- #main -->
+</div><!-- #primary -->
 
-<?php 
+<?php
 get_footer();
